b763a1608f0de5be928d8548e8e44aafe2780552
unknown <enbake@.(none)>
2015-06-27 19:49:01 +0530

    add documentation

--- src/com/animationtwitter/MainActivity.java
+++ src/com/animationtwitter/MainActivity.java
@@ -11,21 +11,64 @@ import android.widget.FrameLayout;
 import android.widget.ListView;
 
 public class MainActivity extends Activity implements OnScrollListener {
+
+	/*
+	 * first of all THRESHOLD_SCROLL variable should be converted in to pixels for all size of android devices see onCreate method. It will
+	 * check and start when user at least scroll by at least 5 density pixels. This value will not varies and remain constant.
+	 */
 	private static int THRESHOLD_SCROLL = 5;
 
+	
+	
 	private ListView mListView;
 	private FrameLayout frameBottom;
 	protected ArrayList<String> arrayList;
 
+
+	/*
+	 * this variable save the last top position of listview while scrolling.
+	 */
 	private int oldTop = -1;
+
+	
+	
 	private int scrollY;
+
+	
+	
+	/*
+	 * this variable save the first visible item on the listview.
+	 */
 	private int oldFirstVisibleItem = -1;
 
+	
+	
+	/*
+	 * this variable check the current scroll difference is equal to the THRESHOLD_SCROLL.
+	 */
 	private int lastScollDiff;
+
+	
+	
+	
+	
+	/*
+	 * variable should be converted in to pixels for all size of android devices. This will set the height of the bottom of the buttons. And
+	 * will appear and disappear on all android devices and not stuck in between the scrolling.
+	 */
 	private int viewHeight = 50;
 
+	
+	
+	
+	/* this will save the last position of scrollY of listview. This value varies on scroll. */
 	private int oldScrollY = 5;
 
+	
+	
+	/*
+	 * this variable should detect whether listview is scrolling up or down.
+	 */
 	private boolean isScrollUp;
 
 	@Override
@@ -42,6 +85,9 @@ public class MainActivity extends Activity implements OnScrollListener {
 
 	}
 
+	/*
+	 * initialize view of widgets
+	 */
 	@SuppressWarnings("rawtypes")
 	private void inItView() {
 
@@ -74,7 +120,8 @@ public class MainActivity extends Activity implements OnScrollListener {
 					scrollY = 0;
 			}
 
-			frameBottom.setTranslationY(scrollY);
+			frameBottom.setTranslationY(scrollY);// this will set the animation of view while appearing and disappearing on scroll state
+													// idle too.
 		}
 	}
 
@@ -82,7 +129,7 @@ public class MainActivity extends Activity implements OnScrollListener {
 	public void onScroll(AbsListView view, int first_visible_child, int visibleItemCount, int totalItemCount) {
 
 		if (first_visible_child == oldFirstVisibleItem) {
-			int top = view.getChildAt(0).getTop();
+			int top = view.getChildAt(0).getTop();// check the top position of listview
 			lastScollDiff = top - oldTop;
 
 			if (lastScollDiff > 0) {
@@ -90,17 +137,17 @@ public class MainActivity extends Activity implements OnScrollListener {
 					scrollY -= Math.abs(lastScollDiff);
 
 				if (scrollY < 0)
-					scrollY = 0;
+					scrollY = 0; // it means animation will appear up
 
-				isScrollUp = true;
+				isScrollUp = true; // this will show scrolling up and animation will visible
 			} else {
 				if (scrollY < viewHeight)
 					scrollY += Math.abs(lastScollDiff);
 
 				if (scrollY > viewHeight)
-					scrollY = viewHeight;
+					scrollY = viewHeight;// this will animation appear down smoothly when scrolly is greater than 0
 
-				isScrollUp = false;
+				isScrollUp = false;// this will show scrolling down and animation will hide
 			}
 			oldTop = top;
 			oldFirstVisibleItem = first_visible_child;
